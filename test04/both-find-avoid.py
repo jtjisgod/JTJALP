e9a5b02c40bf3980a9e17af013847b2074bb59ad
Jangtaejin <jtjisgod@gmail.com>
2018-02-23 16:10:47 +0900

    inputing avoid address can reduce running time.

---
+++ test04/both-find-avoid.py
@@ -0,0 +1,17 @@
+import angr
+import json
+import time
+
+
+def main():
+    proj = angr.Project("jtjalp_example.elf", load_options={"auto_load_libs": False})
+    sm = proj.factory.simulation_manager()
+    print sm.explore(find=(0x4006CB,), avoid=(0x40068B, 0x40068B))
+    return sm.found
+
+start = time.time()
+
+for i in range(0, 10) :
+    founded = main()    
+
+print "Time : " + str(time.time()-start)
